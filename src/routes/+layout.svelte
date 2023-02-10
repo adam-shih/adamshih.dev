94e33735a2b48809be5f224e89dce6c83e3c4fe7
Adam <shihadam1@gmail.com>
2023-02-09 16:10:53 -0800

    Getting started

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+	import pico from '@picocss/pico';
+</script>
+
+<body class="container">
+	<nav>
+		<a href="/">/</a>
+		<a href="/projects">Projects</a>
+		<a href="/resume">Resume</a>
+		<a href="/contact">Contact</a>
+	</nav>
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>&copy; Adam Shih 2023</footer>
+</body>
+
+<style>
+	a {
+		text-align: center;
+	}
+
+	nav {
+		gap: 3rem;
+		justify-content: center;
+		margin-top: 1rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>
+	Adam Shih <br />
+	Software Engineer
+</h1>
+<div id="content">
+	<p>
+		Some words about me. I am super cool and you should agree. Cool! Also, did I mention the
+		ingredients of pizza? It has marinara sauce, mozzarella cheese, dough, and pepperoni. Delicious.
+	</p>
+	<div id="links">
+		<a href="https://www.linkedin.com/in/shihadam">linkedin.com/in/shihadam</a>
+		<a href="https://www.github.com/adam-shih">github.com/adam-shih</a>
+	</div>
+</div>
+
+<style>
+	h1 {
+		text-align: center;
+	}
+
+	#content {
+		display: grid;
+		grid-template-columns: 70% 30%;
+		gap: 3rem;
+	}
+
+	#links {
+		display: flex;
+		flex-direction: column;
+	}
+</style>
