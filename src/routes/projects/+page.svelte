312ea58e917e72c72cec55a886c34da4fb39ec8d
Adam <shihadam1@gmail.com>
2023-02-19 14:29:58 -0800

    Project links

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,11 +2,19 @@
 	<header>
 		<a href="/projects/falling-sand">Falling Sand Simulation</a>
 	</header>
-	<p>A cellular automata to simulate various elements' physics and interactions.</p>
-	<p>
-		Written in Rust and built with the {' '} <a href="https://bevyengine.org">Bevy Game Engine</a>.
-		Compiled into WebAssembly to run in the browser!
-	</p>
+	<body>
+		<p>
+			<a href="/projects/falling-sand">Live Demo</a>
+			&#x2022;
+			<a href="https://github.com/adam-shih/falling-sand">GitHub Repository</a>
+		</p>
+		<p>A cellular automata to simulate various elements' physics and interactions.</p>
+		<p>
+			Written in Rust and built with the {' '}
+			<a href="https://bevyengine.org">Bevy Game Engine</a>. Compiled into WebAssembly to run in the
+			browser!
+		</p>
+	</body>
 </article>
 
 <style>
