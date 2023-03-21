985817931fb563c76640ec6b2c9157ffeb85cbba
Adam <shihadam1@gmail.com>
2023-03-20 21:15:31 -0700

    Code organization

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,21 @@
+<footer>
+	<small
+		>&copy; Adam Shih 2023 &#x2022; Built with <a
+			class="footer-link"
+			href="https://kit.svelte.dev/"
+			target="_blank"
+			rel="noopener noreferrer">SvelteKit</a
+		>
+		and
+		<a class="footer-link" href="https://picocss.com/" target="_blank" rel="noopener noreferrer"
+			>Pico.css</a
+		>!</small
+	>
+</footer>
+
+<style>
+	footer {
+		text-align: center;
+		margin: 2rem 0;
+	}
+</style>
