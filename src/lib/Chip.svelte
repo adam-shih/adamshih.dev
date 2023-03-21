a8ea8d7af8ae9c90d44b2bbd29f57b33f04d84fd
Adam <shihadam1@gmail.com>
2023-03-20 21:02:44 -0700

    Style changes

---
+++ src/lib/Chip.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let text;
+</script>
+
+<div id="chip">{text}</div>
+
+<style>
+	#chip {
+		background: var(--secondary);
+		display: inline-block;
+		padding: 0.125rem 0.5rem;
+		border-radius: var(--border-radius);
+		margin-right: 0.5rem;
+		margin-bottom: 0.5rem;
+		color: #bbc6ce;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,34 @@
+<script>
+	import Chip from '$lib/Chip.svelte';
+
+	const interests = [
+		'Backend Development',
+		'Embedded Systems',
+		'Game Development',
+		'Software Security',
+		'Rust Programming Language'
+	];
+</script>
+
 <header>
 	<h1>Adam Shih</h1>
 	<p>Software Engineer</p>
 </header>
+
 <div>
 	<p>
-		A passionate developer in persuit of continuous learning, best practices, and personal
+		A passionate developer in pursuit of continuous learning, best practices, and personal
 		improvement.
 	</p>
-	<p>
-		<strong>Technical Interests:</strong> Rust, Backend Development, Embedded Systems, Game Development,
-		Software Security
-	</p>
-	<p>Other Hobbies: Digital Art, Snowboarding</p>
+
+	<div id="interests">
+		<strong id="label">Technical Interests:</strong>
+		<span id="chips">
+			{#each interests as interest}
+				<Chip text={interest} />
+			{/each}
+		</span>
+	</div>
 </div>
 
 <style>
@@ -21,5 +38,16 @@
 
 	h1 {
 		margin: 0;
+	}
+
+	#label {
+		flex-shrink: 0;
+		color: var(--secondary);
+	}
+
+	#interests {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
 	}
 </style>
